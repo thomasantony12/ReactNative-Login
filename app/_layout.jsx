533843bbd24897e670160a369d8598fd3572668d
Thomas antony <thomasantony12042001@gmail.com>
2024-12-05 14:32:01 +0530

    welcome page
    UI complete

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
+import { styles } from "@/constants/Colors";
+import { Text, Image, StyleSheet, StatusBar, View, TouchableOpacity } from "react-native";
 import "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
-      <Text style={{color:'red', fontSize:30, fontWeight:900}}>Hi Thomas</Text>
-    </View>
+    <SafeAreaView style={[styles.bg]}>
+      <StatusBar backgroundColor={"#7B71F9"} />
+      <View style={[styles.center]}>
+        <Text style={[styles.heading, styles.shadowProp]}>
+          Let's Get Started!
+        </Text>
+        <Image
+          source={require("../assets/images/logo.png")}
+          style={[styles.logo]}
+        />
+      </View>
+      <View style={[styles.center]}>
+        <TouchableOpacity style={[styles.button]}>
+        <Text style={[styles.text]}>SignUp</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={[{flexDirection:'row', justifyContent:'center', marginTop:10}]}>
+        <Text style={{color:'white'}}>Already have an account?</Text>
+        <TouchableOpacity>
+        <Text style={{color:'#FCC737'}}> Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
+
+
---
+++ constants/Colors.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#7B71F9",
+  },
+
+  text:{
+    fontSize:20,
+    color:"#091057",
+    fontWeight:'700',
+    textAlign:'center',
+  },
+
+  heading: {
+    marginTop: 150,
+    color: "white",
+    fontSize: 40,
+    fontWeight: "900",
+    marginBottom: 13,
+  },
+
+  logo: {
+    marginTop: 50,
+    width: 400,
+    height: 400,
+  },
+
+  center:{
+    alignItems:'center'
+  },
+
+  button: {
+    width:400,
+    height:50,
+    padding:10,
+    borderRadius: 15,
+    backgroundColor:"#FCC737",
+    marginTop: 100,
+  }
+});
